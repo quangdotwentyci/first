3e8a0218c59d45d39fe605043e7dcd01a9dcc5b6
Quang Do <quang.do@twentyci.asia>
2019-09-10 14:57:41 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/broad', 'BroadcastController@index');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
